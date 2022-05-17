8d555163df3bf271d1169665200ddbd7b61c4f22
SushiWaUmai <eugene.bb1@gmail.com>
2022-05-17 18:57:17 +0200

    feat: :sparkles: update player script

--- Assets/Scripts/Entity/Player/PlayerAnimation.cs
+++ Assets/Scripts/Entity/Player/PlayerAnimation.cs
@@ -5,9 +5,10 @@ public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private BasicMovementController2D _playerController;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
     [SerializeField] private string _speedParameter = "speed";
-    [SerializeField] private string _jumpTrigger = "jump";
+    [SerializeField] private string _jumpParameter = "jump";
 
     private void OnValidate()
     {
@@ -16,16 +17,23 @@ public class PlayerAnimation : MonoBehaviour
 
         if (!_playerController)
             _playerController = GetComponent<BasicMovementController2D>();
+
+        if (!_spriteRenderer)
+            _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
     {
-        _playerController.OnJump += OnJump;
+        _playerController.OnJump += OnStartJump;
+        _playerController.OnLanded += OnEndJump;
+        _playerController.OnFacingFlip += UpdateFlip;
     }
 
     private void OnDisable()
     {
-        _playerController.OnJump -= OnJump;
+        _playerController.OnJump -= OnStartJump;
+        _playerController.OnLanded -= OnEndJump;
+        _playerController.OnFacingFlip -= UpdateFlip;
     }
 
     private void FixedUpdate()
@@ -41,12 +49,28 @@ public class PlayerAnimation : MonoBehaviour
         _animator.SetFloat(_speedParameter, Mathf.Abs(_playerController.InputVelocity.x));
     }
 
-    public void OnJump()
+    public void OnStartJump()
     {
-        if (string.IsNullOrEmpty(_jumpTrigger))
+        if (string.IsNullOrEmpty(_jumpParameter))
             return;
 
-        _animator.SetTrigger(_jumpTrigger);
+        _animator.SetBool(_jumpParameter, true);
+    }
+
+    public void OnEndJump()
+    {
+        if (string.IsNullOrEmpty(_jumpParameter))
+            return;
+
+        _animator.SetBool(_jumpParameter, false);
+    }
+
+    public void UpdateFlip(int direction)
+    {
+        if (direction > 0)
+            _spriteRenderer.flipX = false;
+        else if (direction < 0)
+            _spriteRenderer.flipX = true;
     }
 }
 
